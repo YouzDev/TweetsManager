00b2af25f2f65ad1521618ca8507f1717d3e5bce
Sofiane <theabstractdev@gmail.com>
2016-02-02 14:58:53 +0100

    hashtags & mentions coloration + standard

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,89 @@
 /* globals $, document */
 
 $(document).ready(function () {
-	$('.delete').click(function (e) {
-		e.preventDefault()
-		$('.ask').show()
+  $('.text').each(function () {
+    parseString($(this))
+  })
 
-		$('.no').click(function (e) {
-			e.preventDefault()
-			$('.layer').hide()
-		});
+  if ($('.tweet').length === 0) {
+    $('h1').html('You have 0 tweets')
+  }
 
-		$('.yes').click(function (e) {
-			e.preventDefault()
-			$('.layer').hide()
-			$.ajax({
-				url: '/delete',
-				type: 'POST',
-				data: {id: $('.delete').parent().html().split('id="')[1].split('"')[0]},
-			})
-			$('.delete').parent().remove()
-		})
-	})
+  $('.delete').click(function (e) {
+    e.preventDefault()
+    $('.ask').show()
 
-	$('#link').click(function (e) {
-		e.preventDefault()
-		$('.new').show()
-	})
+    $('.no').click(function (e) {
+      e.preventDefault()
+      $('.layer').hide()
+    })
 
-	$('.newTweet-cancel').click(function (e) {
-		e.preventDefault()
-		$('.new').hide()
-	})
+    $('.yes').click(function (e) {
+      e.preventDefault()
+      $('.layer').hide()
+      $.ajax({
+        url: '/delete',
+        type: 'POST',
+        data: {
+          id: $('.delete').parent().html().split('id="')[1].split('"')[0]
+        }
+      })
+      setTimeout(function () {
+        window.location = '/'
+      }, 1000)
+    })
+  })
 
-	$('.newTweet').submit(function (e) {
-		e.preventDefault()
-		if ($('.edit-input').length <= 140) {			
-			$.ajax({
-				url: '/tweet',
-				type: 'POST',
-				data: {status: $('.tweet-status').val()},
-			})
-			$('.new').hide()
-			window.location = '/'
-		} else {
-      $('.tweet-button').before('<p class="error">The tweet must be < of 140 characters</>');
+  $('#link').click(function (e) {
+    e.preventDefault()
+    $('.new').show()
+  })
+
+  $('.newTweet-cancel').click(function (e) {
+    e.preventDefault()
+    $('.new').hide()
+  })
+
+  $('.newTweet').submit(function (e) {
+    e.preventDefault()
+    var status = $('.tweet-status').val()
+    if (status.length <= 140) {
+      $.ajax({
+        url: '/tweet',
+        type: 'POST',
+        data: {
+          status: status
+        }
+      })
+      setTimeout(function () {
+        window.location = '/'
+      }, 1000)
+    } else {
+      $('.tweet-button').before('<p class="error">Status is over 140 characters.</>')
     }
   })
 })
+
+function parseString (text) {
+  var hashtagRegex = /^#\w+|#\w+|#\w+$/
+  var atRegex = /^@\w+|@\w+|@\w+$/
+  var id
+  var textStart
+  var textEnd
+
+  if (hashtagRegex.test(text.html())) {
+    var hashtag = hashtagRegex.exec(text.html())[0]
+    id = text.attr('id')
+    textStart = text.html().split(hashtag)[0]
+    textEnd = text.html().split(hashtag)[1]
+    $('#' + id).html(textStart + '<span class="colored">' + hashtag + '</span>' + textEnd)
+  }
+
+  if (atRegex.test(text.html())) {
+    var at = atRegex.exec(text.html())[0]
+    id = text.attr('id')
+    textStart = text.html().split(at)[0]
+    textEnd = text.html().split(at)[1]
+    $('#' + id).html(textStart + '<span class="colored">' + at + '</span>' + textEnd)
+  }
+}
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var rp = require('request-promise')
-var Twitter = require("twitter")
+var Twitter = require('twitter')
 var bodyParser = require('body-parser')
 var express = require('express')
 var session = require('express-session')
 var hbs = require('hbs')
 var path = require('path')
-var OAuth = require('oauth');
+var _ = require('lodash')
+var fs = require('fs')
+var OAuth = require('oauth')
 var app = express()
 var md5 = require('md5')
-var oauth_token = ""
-var oauth_secret = ""
-var oauth_verifier = ""
-var access_token = ""
-var access_token_secret = ""
+var oauth_token = ''
+var oauth_secret = ''
+var oauth_verifier = ''
+var access_token = ''
+var access_token_secret = ''
 var twitter
 var tweets
 var sess = {
@@ -22,18 +23,15 @@ var sess = {
   saveUninitialized: true
 }
 
-var _ = require('lodash')
-var fs = require('fs')
-
 var oauth = new OAuth.OAuth(
-  "https://twitter.com/oauth/request_token",
-  "https://twitter.com/oauth/access_token",
+  'https://twitter.com/oauth/request_token',
+  'https://twitter.com/oauth/access_token',
   'J3VLchWRKjaMhvuPLFFv9od9H',
   'cLm62xSeK29xODLRVJI5BCi3Es3tXWs9UuvMuVqLaiHuS0UGLP',
-  "1.0A",
+  '1.0A',
   'http://localhost:8080/oauth/authorize',
-  "HMAC-SHA1"
-);
+  'HMAC-SHA1'
+)
 
 hbs.registerHelper('assets', (process.env.NODE_ENV === 'production' ? _.memoize : _.identity)(function (path) {
   var file = fs.readFileSync('assets' + path, 'utf8')
@@ -42,8 +40,10 @@ hbs.registerHelper('assets', (process.env.NODE_ENV === 'production' ? _.memoize 
 
 app.use('/assets/', express.static(path.join(__dirname, '/assets')))
 app.use(session(sess))
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'hbs')
 
@@ -58,49 +58,60 @@ app.get('/oauth/request_token', function (req, res, next) {
 })
 
 app.get('/oauth/authorize', function (req, res) {
-  oauth_token = req.query.oauth_token
-  oauth_secret = req.query.oauth_secret
-  oauth_verifier = req.query.oauth_verifier
+  if (!req.query.denied) {
+    oauth_token = req.query.oauth_token
+    oauth_secret = req.query.oauth_secret
+    oauth_verifier = req.query.oauth_verifier
 
-	oauth.getOAuthAccessToken(oauth_token, oauth_secret, oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
-    if (error) {
-      console.log(error)
-    } else {
-      access_token = oauthAccessToken
-      access_token_secret = oauthAccessTokenSecret
+    oauth.getOAuthAccessToken(oauth_token, oauth_secret, oauth_verifier, function (error, oauthAccessToken, oauthAccessTokenSecret, results) {
+      if (error) {
+        console.log(error)
+      } else {
+        access_token = oauthAccessToken
+        access_token_secret = oauthAccessTokenSecret
 
-      twitter = new Twitter({
-        consumer_key: 'J3VLchWRKjaMhvuPLFFv9od9H',
-        consumer_secret: 'cLm62xSeK29xODLRVJI5BCi3Es3tXWs9UuvMuVqLaiHuS0UGLP',
-        access_token_key: access_token,
-        access_token_secret: access_token_secret
-      });
-      
-      twitter.get('account/verify_credentials', function (error, data){
-        if(error) {
-          console.log(error)
-        } else {
-          req.session.userId = data["id"]
-          req.session.username = data["screen_name"]
-          req.session.name = data["name"]
-          req.session.profilePic = data["profile_image_url"].split('_normal')[0] + '.png'
-        }
-        res.redirect('/')
-      })
-      
-    }
-  });
+        twitter = new Twitter({
+          consumer_key: 'J3VLchWRKjaMhvuPLFFv9od9H',
+          consumer_secret: 'cLm62xSeK29xODLRVJI5BCi3Es3tXWs9UuvMuVqLaiHuS0UGLP',
+          access_token_key: access_token,
+          access_token_secret: access_token_secret
+        })
+
+        twitter.get('account/verify_credentials', function (error, data) {
+          if (error) {
+            console.log(error)
+          } else {
+            req.session.userId = data.id
+            req.session.username = data.screen_name
+            req.session.name = data.name
+            req.session.profilePic = data.profile_image_url.split('_normal')[0] + '.png'
+          }
+          res.redirect('/')
+        })
+      }
+    })
+  } else {
+    res.render('error', {
+      message: 'You canceled the authentication. Redirecting to home'
+    })
+  }
 })
 
 app.get('/', function (req, res) {
   if (req.session.username) {
-    twitter.get('statuses/user_timeline', function (error, data){
-      if(error) {
-        console.log(error)
+    twitter.get('statuses/user_timeline', function (error, data) {
+      if (error) {
+        res.render('error', {
+          message: error
+        })
       } else {
         tweets = data
       }
-      res.render('index', {username: req.session.username, profile_picture: req.session.profilePic, tweets: tweets})
+      return res.render('index', {
+        username: req.session.username,
+        profile_picture: req.session.profilePic,
+        tweets: tweets
+      })
     })
   } else {
     res.render('signin')
@@ -108,19 +119,21 @@ app.get('/', function (req, res) {
 })
 
 app.get('/logout', function (req, res) {
-  req.session.userId = null;
-  req.session.username = null;
-  req.session.name = null;
-  req.session.profilePic = null;
+  req.session.userId = null
+  req.session.username = null
+  req.session.name = null
+  req.session.profilePic = null
   res.redirect('/')
 })
 
 app.post('/tweet', function (req, res) {
-  twitter.post('statuses/update', {status: req.body.status}, function (error, data) {
-    if(error) {
-      console.log(error)
-    } else {
-      tweets = data
+  twitter.post('statuses/update', {
+    status: req.body.status
+  }, function (error, data) {
+    if (error) {
+      res.render('error', {
+        message: error
+      })
     }
   })
 })
@@ -135,35 +148,13 @@ app.post('/tweet', function (req, res) {
 
 app.post('/delete', function (req, res) {
   twitter.post('statuses/destroy/' + req.body.id, function (error, data) {
-    if(error) {
-      console.log(error)
+    if (error) {
+      res.render('error', {
+        message: error
+      })
     }
   })
 })
-
-// app.get('/feed', function (req, res) {
-//   if (req.session.userId) {   
-//     console.log(req.query)
-//     if (req.query.search) {
-//       twitter.get('statuses/user_timeline', {screen_name: req.query.search}, function (error, data){
-//         console.log("get")
-//         if(error) {
-//           console.log(error)
-//         } else {
-//           res.render('feed', {tweets: data})
-//         }
-//       })
-//     } else {
-//       res.render('error', {
-//         message: "no params"
-//       })
-//     }
-//   } else {
-//     res.render('error', {
-//       message: "You must sign in with twitter to access to this page"
-//     })
-//   }
-// })
 
 app.get('*', function (req, res, next) {
   var err = new Error()
@@ -171,7 +162,6 @@ app.get('*', function (req, res, next) {
   err.message = 'Wrong address'
   next(err)
 })
-
 
 app.use(function (err, req, res, next) {
   if (err.status !== 404) {
@@ -185,5 +175,5 @@ app.use(function (err, req, res, next) {
 })
 
 app.listen(8080, function () {
-  console.log('Server running');
+  console.log('Server running')
 })
