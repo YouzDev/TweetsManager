5f3de01a1ccda7e9ba2e056a09a26643facba92c
Sofiane <theabstractdev@gmail.com>
2016-02-04 11:02:19 +0100

    verified ip

--- index.js
+++ index.js
@@ -165,5 +165,10 @@ app.use(function (err, req, res, next) {
 })
 
 app.listen(8080, function () {
-  console.log('Server running on http://' + ip + ':8080')
+  if (ip === 'localhost' || ip[0] === '1') {
+    console.log('Server running on http://' + ip + ':8080')
+  } else if (ip !== 'localhost' && ip[0] !== '1') {
+    console.error('Your ip address is not valid')
+    process.exit(1)
+  }
 })
